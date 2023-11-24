17550a2cafc35b2d2eee6b04d1333ae303f7d16f
Andy Jenkins <andrewajenkins@outlook.com>
2023-11-23 20:02:38 -0800

    add regional inventory control

--- simulation.py
+++ simulation.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 
 
 class ManufacturingFacility:
-    def __init__(self, env, production_rate, capacity):
+    def __init__(self, env, destination, production_rate, capacity, transit_times):
         self.env = env
+        self.destination = destination
+        self.transit_times = transit_times
         self.production_rate = production_rate  # units per day
         self.capacity = simpy.Container(
             env, init=0, capacity=capacity
@@ -32,14 +34,19 @@ class ManufacturingFacility:
 
 
 class StorageFacility:
-    def __init__(self, env, name, initial_inventory, capacity):
+    def __init__(
+        self, env, name, destination, initial_inventory, capacity, transit_times
+    ):
         self.env = env
         self.name = name
+        self.destination = destination
+        self.transit_times = transit_times
         self.inventory = simpy.Container(
             env, init=initial_inventory, capacity=capacity
         )  # Drug inventory
         self.inventory_log = []
         self.log_inventory(0)  # Log initial state
+        self.on_order = 0
 
     def order_drug(self, quantity):
         """Processes an order and waits if necessary for stock to be available."""
@@ -51,12 +58,27 @@ class StorageFacility:
         yield self.inventory.put(quantity)
         print(f"Received an order of {quantity} units of the drug. {self.env.now}")
         self.log_inventory(self.env.now)
+        # Once received, update the `on_order` to reflect that this order has been fulfilled
+        self.on_order -= quantity
 
     def dispatch_drug(self, quantity):
-        """Dispatch drugs for transportation."""
-        yield self.inventory.get(quantity)
-        print(f"Dispatched {quantity} units for drug on day {self.env.now}.")
-        self.log_inventory(self.env.now)
+        # Check if we have enough drugs in inventory to fulfill the dispatch
+        if self.inventory.level >= quantity:
+            yield self.inventory.get(
+                quantity
+            )  # Update inventory to reflect the dispatch
+            print(f"Dispatched {quantity} units for drug on day {self.env.now}.")
+            self.log_inventory(self.env.now)
+            # Assuming a function to handle transportation and reception of the drugs at the destination
+            yield self.env.process(
+                transport_drug(
+                    self.env, self, self.destination, quantity, self.transit_times
+                )
+            )
+        else:
+            print(
+                f"Not enough inventory to dispatch {quantity} units on day {self.env.now}."
+            )
 
     def log_inventory(self, time):
         """Logs the current inventory level along with the simulation time."""
@@ -65,24 +87,34 @@ class StorageFacility:
 
 # Subclass for RegionalStorageFacility
 class RegionalStorageFacility(StorageFacility):
-    def __init__(self, env, name, initial_inventory, capacity):
-        super().__init__(env, name, initial_inventory, capacity)
+    def __init__(
+        self, env, name, destination, initial_inventory, capacity, transit_times
+    ):
+        super().__init__(
+            env, name, destination, initial_inventory, capacity, transit_times
+        )
 
 
 # Subclass for CentralStorageFacility
 class CentralStorageFacility(StorageFacility):
-    def __init__(self, env, name, initial_inventory, capacity):
-        super().__init__(env, name, initial_inventory, capacity)
+    def __init__(
+        self, env, name, destination, initial_inventory, capacity, transit_times
+    ):
+        super().__init__(
+            env, name, destination, initial_inventory, capacity, transit_times
+        )
 
 
 class TrialSite:
     def __init__(self, env, initial_inventory, dosage_schedule, capacity):
         self.env = env
+        self.name = "trial_site"
         self.patients = 0
         self.dosage_schedule = dosage_schedule  # Dosage per patient
         self.inventory = simpy.Container(env, init=initial_inventory, capacity=capacity)
         self.inventory_log = []
         self.log_inventory(0)  # Log initial state
+        self.on_order = 0
 
     def request_resupply(self, storage_facility, quantity):
         """Requests a resupply of drugs from a storage facility."""
@@ -184,10 +216,74 @@ class DosageSchedule:
         return self.next_dose_time
 
 
+class InventoryController:
+    def __init__(
+        self,
+        env,
+        facility,
+        supplier,
+        reorder_point,
+        reorder_quantity,
+        lead_time,
+        forecast_window,
+    ):
+        self.env = env
+        self.facility = facility
+        self.supplier = supplier
+        self.reorder_point = reorder_point
+        self.reorder_quantity = reorder_quantity
+        self.lead_time = lead_time
+        self.forecast_window = (
+            forecast_window  # Looking ahead 'forecast_window' days to anticipate demand
+        )
+        self.env.process(self.run())
+
+    def run(self):
+        while True:
+            forecasted_demand = self.forecast_demand()
+            projected_inventory = (
+                self.facility.inventory.level
+                + self.facility.on_order
+                - forecasted_demand
+            )
+            if projected_inventory < self.reorder_point:
+                yield self.env.process(self.place_order())
+            yield self.env.timeout(1)  # Check inventory periodically
+
+    def place_order(self):
+        # Include logic to handle if the supplier cannot fulfill the entire order quantity
+        print(
+            f"Placing order for {self.reorder_quantity} units from {self.supplier.name}"
+        )
+        yield self.env.timeout(self.lead_time)  # Wait for the lead time
+        # yield self.env.process(self.supplier.order_drug(self.reorder_quantity))
+        yield self.env.process(self.supplier.dispatch_drug(self.reorder_quantity))
+        # It may be useful to track quantities that have been ordered but not yet received
+        self.facility.on_order += self.reorder_quantity
+
+    def forecast_demand(self):
+        # Simple demand forecast: average usage multiplied by forecast window
+        # You can replace this with more complex logic as needed
+        average_daily_usage = self.calculate_average_daily_usage()
+        return average_daily_usage * self.forecast_window
+
+    def calculate_average_daily_usage(self):
+        # Example logic for average daily usage
+        # This could be based on historical usage data or other forecasting models
+        # For now, we simply divide total usage by the number of days
+        total_usage = sum(
+            amount for _, amount in self.facility.inventory_log
+        )  # Sum all the usage
+        days_passed = self.env.now
+        return total_usage / days_passed if days_passed > 0 else 0
+
+
 def transport_drug(env, source, destination, quantity, transit_times):
     """Simulates the transportation of drugs."""
     default_time = transit_times["default"]
-    transport_time = transit_times.get(source + "_to_" + destination, default_time)
+    transport_time = transit_times.get(
+        source.name + "_to_" + destination.name, default_time
+    )
     yield env.timeout(transport_time)  # Represents transport delay
     source.dispatch_drug(quantity)  # Get drugs from source
     yield env.process(destination.receive_drug(quantity))  # Add drugs to destination
@@ -309,26 +405,6 @@ def run_simulation():
     with open("data.yml", "r") as file:
         config = yaml.safe_load(file)["trial_a"]
 
-    # Instantiate resources
-    manufacturer = ManufacturingFacility(
-        env,
-        production_rate=config["manufacturer"]["prod_rate"],
-        capacity=config["manufacturer"]["capacity"],
-    )
-    central_storage = CentralStorageFacility(
-        env,
-        "central_storage",
-        initial_inventory=config["central_storage"]["initial_inventory"],
-        capacity=config["central_storage"]["capacity"],
-    )
-    regional_storage = RegionalStorageFacility(
-        env,
-        "regional_storage",
-        initial_inventory=config["regional_storage"]["initial_inventory"],
-        capacity=config["regional_storage"]["capacity"],
-    )
-
-    # Define your dosage schedule and trial site here
     dosage_schedule = DosageSchedule(
         dose_amount=config["dosage"]["amount"],
         dose_interval=config["dosage"]["interval"],
@@ -338,6 +414,48 @@ def run_simulation():
         initial_inventory=config["site"]["initial_inventory"],
         dosage_schedule=dosage_schedule,
         capacity=config["site"]["capacity"],
+    )
+    regional_storage = RegionalStorageFacility(
+        env,
+        "regional_storage",
+        destination=trial_site,
+        initial_inventory=config["regional_storage"]["initial_inventory"],
+        capacity=config["regional_storage"]["capacity"],
+        transit_times=config["transit_times"],
+    )
+    central_storage = CentralStorageFacility(
+        env,
+        "central_storage",
+        destination=regional_storage,
+        initial_inventory=config["central_storage"]["initial_inventory"],
+        capacity=config["central_storage"]["capacity"],
+        transit_times=config["transit_times"],
+    )
+    manufacturer = ManufacturingFacility(
+        env,
+        destination=central_storage,
+        production_rate=config["manufacturer"]["prod_rate"],
+        capacity=config["manufacturer"]["capacity"],
+        transit_times=config["transit_times"],
+    )
+
+    regional_storage_inventory_controller = InventoryController(
+        env=env,
+        facility=regional_storage,
+        supplier=central_storage,
+        reorder_point=config["regional_storage"]["reorder_point"],
+        reorder_quantity=config["regional_storage"]["reorder_quantity"],
+        lead_time=config["transit_times"]["central_storage_to_regional_storage"],
+        forecast_window=3,
+    )
+    trial_site_inventory_controller = InventoryController(
+        env=env,
+        facility=trial_site,
+        supplier=regional_storage,
+        reorder_point=config["site"]["reorder_point"],
+        reorder_quantity=config["site"]["reorder_quantity"],
+        lead_time=config["transit_times"]["regional_storage_to_trial_site"],
+        forecast_window=3,
     )
 
     # Schedule the initial distribution of drugs from manufacturer to central storage
@@ -351,7 +469,7 @@ def run_simulation():
         )
     )
 
-    # Schedule distribution from central storage to regional storage
+    # Schedule the initial distribution from central storage to regional storage
     env.process(
         distribute_drug_storage_to_storage(
             env,
@@ -362,16 +480,16 @@ def run_simulation():
         )
     )
 
-    env.process(
-        inventory_control(
-            env,
-            trial_site,
-            central_storage,
-            config["inv_control"]["reorder_point"],
-            config["inv_control"]["reorder_quantity"],
-            config["inv_control"]["lead_time"],
-        )
-    )
+    # env.process(
+    #     inventory_control(
+    #         env,
+    #         trial_site,
+    #         central_storage,
+    #         config["inv_control"]["reorder_point"],
+    #         config["inv_control"]["reorder_quantity"],
+    #         config["inv_control"]["lead_time"],
+    #     )
+    # )
 
     # Start the enrollment process
     env.process(
